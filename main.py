a7cc7b519246af06795f094a93e3531faab1dae7
dmedina236 <dmedina23671@gmail.com>
2025-04-25 17:30:12 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import Operaciones.matematica as mat
-import Operaciones.geometria as geo
-
-print(mat.multiplicacion(10, 5))
-print(mat.division(10, 5))
-print(geo.area_rectangulo(10, 5))
-print(geo.perimetro_rectangulo(10, 5))
